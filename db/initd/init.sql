2529a40b3efd48b6678b04fa0b133cf1b4573da0
井上碧 <inoueaoi@aoi.local>
2020-12-05 18:44:03 +0900

    update

--- db/initd/init.sql
+++ db/initd/init.sql
@@ -3,7 +3,9 @@ CREATE TABLE users_db.users (
 id bigint(20) not null auto_increment,
 first_name varchar(45),
 last_name varchar(45),
-email varchar(45) not null ,
-date_created varchar(45),
+email varchar(45) not null,
+date_created datetime not null,
+status varchar(45) not null,
+password varchar(32) not null,
 primary key (id),
 unique index email_UNIQUE (email asc));
